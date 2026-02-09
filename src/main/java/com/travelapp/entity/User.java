77b3f0730a428b61010d2a169a21aa39f7f358bb
Paulo <pborkart@outlook.com>
2026-02-09 11:13:20 -0400

    changes

--- src/main/java/com/travelapp/entity/User.java
+++ src/main/java/com/travelapp/entity/User.java
@@ -44,7 +44,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(
+        return List.of(
                 new SimpleGrantedAuthority("ROLE_" + role.name())
         );
     }
